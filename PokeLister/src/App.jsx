c43e466a9d0183f00aff56d504be26149f7d9ab6
hravv <harveyoliverburman@outlook.com>
2025-11-12 21:44:17 +0000

    implemented functionality

---
+++ PokeLister/src/App.jsx
@@ -0,0 +1,83 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import pokeball from './assets/pokeball.png'
+import title from './assets/pokelister.png'
+import './App.css'
+
+function App() {
+
+  const [input, setInput] = useState("");
+  const [chosenPokemon, setChosenPokemon] = useState("");
+  const [chosenImg, setChosenImg] = useState("");
+  const [pokemonList, setPokemonList] = useState([]);
+  const [nameValid, setNameValid] = useState(true);
+
+  class Pokemon {
+    constructor (name, img) {
+      this.name = name;
+      this.img = img;
+    }
+  }
+
+  async function fetchPokemon(input) {
+    try {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${input}`)
+      if (!response.ok) {
+        setNameValid(false);
+        throw new Error('Network response was not ok')
+      }
+      const data = await response.json();
+      setNameValid(true);
+      updatePokemonInfo(data);
+    } catch (error) {
+      console.error("There was a problem with the fetch operation")
+    }
+  }
+
+  function updatePokemonInfo (data) {
+      setChosenImg(data.sprites.front_default);
+      setChosenPokemon(capitalise(data.name));
+  }
+  function capitalise(input) {
+    return String(input).charAt(0).toUpperCase() + String(input).slice(1);
+  }
+
+  return (
+    <>
+      <div className="title">
+      {/*<a href="https://fontmeme.com/pokemon-font/"><img src="https://fontmeme.com/permalink/250520/3d0199ed9ade02ff11817812eec9bc42.png" alt="pokemon-font" border="0" /></a>*/}
+      <img src={title} className='w-fit'/>
+      </div>
+      <section className='maincontent'>
+        <div>
+          <p className=''>Enter the name of a Pokémon and add it to a list of your favourites!</p>
+          <input type="text" id='textinput' value={input} onChange={(e)=>{setInput(e.target.value)}}/>
+          <button type='submit' id='submit' onClick={()=>fetchPokemon(input)}>Enter</button>
+        </div>
+        <div>
+          <figure id='prevfigure'>
+            <div id='img-bg'>
+              <img src={nameValid ? (chosenPokemon ? chosenImg : pokeball) : pokeball} id="previmage" className="w-[120px] h-[120px] object-contain" />
+            </div>
+            <figcaption>{nameValid ? (chosenPokemon ? chosenPokemon : "No Pokémon Selected") : "Invalid Name!"}</figcaption>
+          </figure>
+          <button id='add' onClick={() => setPokemonList([...pokemonList , new Pokemon(chosenPokemon, chosenImg)])}>Add</button>
+        </div>
+        <div>
+          <div id='img-grid'>{pokemonList.map((Pokemon, index)=> {
+            return (
+              <figure key={index}>
+              <img src={Pokemon.img} alt={Pokemon.name} />
+              <figcaption>{Pokemon.name}</figcaption>
+            </figure>
+            )
+          }
+          )}</div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
